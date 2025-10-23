01ea6b3126766cfb3db2518210f8a74c48ee9d50
bonillab <nestorbonillabrenes@gmail.com>
2025-10-23 16:51:42 -0600

    add first name and tenant_id on user register

--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,11 @@ class BlockIntrospectionForAnonymous(SchemaExtension):
     def on_request_start(self):
         request = self.execution_context.context["request"]
         user = getattr(request, "user", None)
-        query_str = self.execution_context.query.strip().lower()
+        query_str = (self.execution_context.query or "").strip().lower()
         if not (user and user.is_authenticated):
             if "__schema" in query_str or "__type" in query_str:
                 raise GraphQLError("Introspection is disabled for unauthenticated users.")
+
+class ROLE_ID:
+    Ambassadors = 1
+    SparkAdmin = 2
