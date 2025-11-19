e79dae3ebabe0b3f9c3b6eda8fb19ec546147939
David Gaitan <jdavid.gaitan@gmail.com>
2025-11-19 10:57:35 -0600

    adding mutations to schemas

--- jobs/schema.py
+++ jobs/schema.py
@@ -6,6 +6,25 @@ from jobs import mutations
 @strawberry.type
 class ClientJobMutations(
     mutations.StatusMutations,
+    mutations.CompanyMutations,
+    mutations.PayTimingMutations,
+    mutations.JobMutations,
+    mutations.ReviewScoreMutations,
+    mutations.JobTitleMutations,
+    mutations.RateTypeMutations,
+    mutations.RateMutations,
+    mutations.JobMutations,
+    mutations.JobFileMutations,
+    mutations.JobRequirementTypeMutations,
+    mutations.JobRequirementMutations,
+    mutations.JobRequirementFileMutations,
+    mutations.AmbassadorJobMutations,
+    mutations.CompanyToAmbassadorReviewMutations,
+    mutations.AmbassadorToAmbassadorReviewMutations,
+    mutations.QuestionTypeMutations,
+    mutations.JobRequirementQuestionMutations,
+    mutations.QuestionOptionMutations,
+    mutations.JobRequirementAnswerMutations,
 ):
     pass
 
